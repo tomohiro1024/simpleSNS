23cd7f25df59a4b2ff53f66f49ddd53c68d0cd2a
tomohiro1024 <gongyezhihong732@gmail.com>
2022-02-26 22:51:20 +0900

    FirebaseAuth作成開始

---
+++ lib/utils/authentication.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authentication {
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  static User? currentFirebaseUser;
+
+  static Future<dynamic> signUp(
+      {required String email, required String pass}) async {}
+}
