53e2b4263fa40d7048ad1dce0aaadfcd2a3f8e1e
tomohiro1024 <gongyezhihong732@gmail.com>
2022-02-27 12:50:07 +0900

    FirebaseAuth作成

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -5,5 +5,15 @@ class Authentication {
   static User? currentFirebaseUser;
 
   static Future<dynamic> signUp(
-      {required String email, required String pass}) async {}
+      {required String email, required String pass}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: pass);
+      print('認証登録OK');
+      return true;
+    } on FirebaseAuthException catch (e) {
+      print('認証登録エラー');
+      return '認証に失敗しました';
+    }
+  }
 }
--- lib/view/start_up/create_account_page.dart
+++ lib/view/start_up/create_account_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:simple_sns/utils/authentication.dart';
 
 class CreateAccountPage extends StatefulWidget {
   const CreateAccountPage({Key? key}) : super(key: key);
@@ -103,14 +104,19 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
               ),
               SizedBox(height: 30),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   if (nameController.text.isNotEmpty &&
                       userIdController.text.isNotEmpty &&
                       serfIntroductionController.text.isNotEmpty &&
                       emailController.text.isNotEmpty &&
                       passwordController.text.isNotEmpty &&
                       image != null) {
-                    Navigator.pop(context);
+                    var result = await Authentication.signUp(
+                        email: emailController.text,
+                        pass: passwordController.text);
+                    if (result == true) {
+                      Navigator.pop(context);
+                    }
                   }
                 },
                 child: Text(
