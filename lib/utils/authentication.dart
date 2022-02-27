9d4a624c965a05492faf3da16ee4b6f4b22e0d92
tomohiro1024 <gongyezhihong732@gmail.com>
2022-02-27 18:11:51 +0900

    ログイン認証

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -4,6 +4,7 @@ class Authentication {
   static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   static User? currentFirebaseUser;
 
+  // アカウント登録
   static Future<dynamic> signUp(
       {required String email, required String pass}) async {
     try {
@@ -12,8 +13,23 @@ class Authentication {
       print('認証登録OK');
       return true;
     } on FirebaseAuthException catch (e) {
-      print('認証登録エラー');
-      return '認証に失敗しました';
+      print('認証登録エラー: $e');
+      return false;
+    }
+  }
+
+  // Eメールログイン
+  static Future<dynamic> emailSignIn(
+      {required String email, required String pass}) async {
+    try {
+      final UserCredential _result = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: pass);
+      currentFirebaseUser = _result.user;
+      print('ログイン成功');
+      return true;
+    } on FirebaseAuthException catch (e) {
+      print('ログイン失敗しました: $e');
+      return false;
     }
   }
 }
--- lib/view/start_up/login_page.dart
+++ lib/view/start_up/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_sns/utils/authentication.dart';
 import 'package:simple_sns/view/screen.dart';
 import 'package:simple_sns/view/start_up/create_account_page.dart';
 
@@ -17,87 +18,96 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 70,
-              ),
-              Text(
-                'シンプルSNS',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.cyan),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 25.0),
-                child: Container(
-                  width: 250,
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(hintText: 'Email'),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 70,
+                ),
+                Text(
+                  'シンプルSNS',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.cyan),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25.0),
+                  child: Container(
+                    width: 250,
+                    child: TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(hintText: 'Email'),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                width: 250,
-                child: TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(hintText: 'Password'),
+                Container(
+                  width: 250,
+                  child: TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(hintText: 'Password'),
+                  ),
                 ),
-              ),
-              SizedBox(height: 15),
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(color: Colors.black87),
-                  children: [
-                    TextSpan(text: 'アカウントを持ってない方は'),
-                    TextSpan(
-                        text: 'こちら',
-                        style: TextStyle(
-                          color: Colors.pinkAccent,
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                  pageBuilder: (context, animation,
-                                          secondaryAnimation) =>
-                                      CreateAccountPage(),
-                                  transitionsBuilder: (context, animation,
-                                      secondaryAnimation, child) {
-                                    return FadeUpwardsPageTransitionsBuilder()
-                                        .buildTransitions(
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    CreateAccountPage()),
-                                            context,
-                                            animation,
-                                            secondaryAnimation,
-                                            child);
-                                  },
-                                ));
-                          }),
-                  ],
+                SizedBox(height: 15),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black87),
+                    children: [
+                      TextSpan(text: 'アカウントを持ってない方は'),
+                      TextSpan(
+                          text: 'こちら',
+                          style: TextStyle(
+                            color: Colors.pinkAccent,
+                            decoration: TextDecoration.underline,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                    pageBuilder: (context, animation,
+                                            secondaryAnimation) =>
+                                        CreateAccountPage(),
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      return FadeUpwardsPageTransitionsBuilder()
+                                          .buildTransitions(
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      CreateAccountPage()),
+                                              context,
+                                              animation,
+                                              secondaryAnimation,
+                                              child);
+                                    },
+                                  ));
+                            }),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => const Screen()));
-                },
-                child: Text('Emailでログイン'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.cyan, //ボタンの背景色
-                ),
-              )
-            ],
+                SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () async {
+                    var result = await Authentication.emailSignIn(
+                        email: emailController.text,
+                        pass: passwordController.text);
+                    if (result == true) {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Screen()));
+                    }
+                  },
+                  child: Text('Emailでログイン'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.cyan, //ボタンの背景色
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
