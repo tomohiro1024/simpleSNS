0149fad2e4147f8325e50e6a58a7e8389ee2b5d8
tomohiro1024 <gongyezhihong732@gmail.com>
2022-03-05 18:41:00 +0900

    postsの実装2

--- lib/utils/firestore/posts.dart
+++ lib/utils/firestore/posts.dart
@@ -33,4 +33,26 @@ class PostsFirestore {
       return false;
     }
   }
+
+  static Future<dynamic> getPostsFromIds(List<String> ids) async {
+    List<Post> postList = [];
+    try {
+      await Future.forEach(ids, (String id) async {
+        DocumentSnapshot doc = await posts.doc(id).get();
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        Post post = Post(
+          id: doc.id,
+          content: data['content'],
+          postAccountId: data['post_account_id'],
+          createdTime: data['created_time'],
+        );
+        postList.add(post);
+      });
+      print('自分の投稿を取得しました');
+      return postList;
+    } on FirebaseException catch (e) {
+      print('自分の投稿を取得できませんでした: $e');
+      return false;
+    }
+  }
 }
